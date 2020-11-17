72ba24189acb11718db8de3f446edbcbd2a1eb35
Red1042 <jhoward1042@gmail.com>
2020-11-17 02:13:13 -0600

    Made M73 GPMG

--- 105th_Weapons/cfg/Machineguns.hpp
+++ 105th_Weapons/cfg/Machineguns.hpp
@@ -97,6 +97,94 @@
 					};
 			};
 
+			class v105_HOW_M73GPMG:OPTRE_M73
+			{
+				displayName					= "[105th] M73 GPMG";
+				baseWeapon 					= "v105_HOW_M73GPMG";
+				descriptionShort="UNSC Squad Automatic Weapon";
+				recoil="recoil_lim";
+				modes[]=
+				{
+					"FullAutoSlow",
+					"FullAutoFast",
+				};
+				class FullAutoSlow: FullAuto
+					{
+						reloadTime=0.15;
+						dispersion=0.000055000001;
+						
+						/*sounds[]=
+						{
+							"StandardSound",
+							"SilencedSound"
+						};*/
+						showToPlayer=1;
+					};
+					class FullAutoFast: FullAutoSlow
+					{
+						reloadTime=0.075;
+						dispersion=0.000055000001;	
+						textureType="fastAuto";
+						
+					};
+					class WeaponSlotsInfo: WeaponSlotsInfo
+					{
+						class MuzzleSlot: MuzzleSlot
+						{
+							iconPosition[]={0,0.40000001};
+						};
+						class CowsSlot: CowsSlot
+						{
+							iconPosition[]={0.5,0.30000001};
+							compatibleitems[]=
+							{
+								"optic_arco_ak_blk_f",
+								
+								"optic_aco_grn",
+								"optic_aco",
+								"optic_arco_blk_f",
+								"optic_holosight_blk_f",
+								"optic_mrco",
+								
+								"optic_hamr",
+							
+								
+								"optic_erco_blk_f",
+								
+									
+								"optic_yorris",
+									
+								"ace_optic_mrco_2d",
+								"ace_optic_hamr_2d",
+								"ace_optic_hamr_pip",
+								
+								"optic_aco_smg",
+								"optic_aco_grn_smg",
+								"optic_holosight_smg_blk_f",
+								
+
+								"optre_m73_smartlink_v2",
+								"OPTRE_M7_Sight_v2",
+								"OPTRE_M392_Scope_v2",
+								"OPTRE_M393_Scope_v2",
+								"OPTRE_M393_ACOG_v2",
+								"OPTRE_M393_EOTECH_v2",
+								"OPTRE_BR55HB_Scope_v2",
+								"OPTRE_BMR_Scope_v2"
+							};
+						};
+						class PointerSlot: PointerSlot
+						{
+							iconPosition[]={0.2,0.40000001};
+						};
+						mass=200;
+					};
+					class ItemInfo
+					{
+						priority=1;
+					};
+			};
+
 			class v105_HOW_M247SSW: OPTRE_M247
 			{
 				dlc="OPTRE";
