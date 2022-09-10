82d426a0828f5d147da57d789344b21f9e311a8b
NestHaven <rot.bote@gmail.com>
2022-09-10 11:52:50 -0400

    105th Utility Changes: +Added Utility.hpp to Sparrowhawk, giving it I-TGT and AMS again-ish

--- 105th_Vehicles/Sparrowhawk_Base.hpp
+++ 105th_Vehicles/Sparrowhawk_Base.hpp
@@ -155,8 +155,7 @@ class v105_Sparrowhawk_Base: OPTRE_AV22_Sparrowhawk_Base
 			};
 			magazines[]=
 			{
-				"OPTRE_100Rnd_50mm_HE",
-				"OPTRE_100Rnd_50mm_APFSDS",
+			    "OPTRE_100Rnd_50mm_HE",
 				"OPTRE_100Rnd_50mm_APFSDS",
 				"OPTRE_100Rnd_50mm_APFSDS",
 				"Laserbatteries"
@@ -250,6 +249,7 @@ class v105_Sparrowhawk_Base: OPTRE_AV22_Sparrowhawk_Base
 					gunnerOpticsEffect[]={};
 				};
 			};
+
 			class Components
 			{
 				class VehicleSystemsDisplayManagerComponentLeft: DefaultVehicleSystemsDisplayManagerLeft
@@ -402,6 +402,7 @@ class v105_Sparrowhawk_Base: OPTRE_AV22_Sparrowhawk_Base
 	};
 	class UserActions
 	{
+        #include "cfg\UtilityActions.hpp"
 		#include "cfg\ThrusterActions.hpp"
 	};
     class Components : Components
